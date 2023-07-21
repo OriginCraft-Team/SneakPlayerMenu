a6097ae72afce2f7ebcb4264161f5ddeb855c2b8
Zhenyuan <69752436+awdrgyj8@users.noreply.github.com>
2023-07-22 00:33:42 +0800

    Suppress SneakPlayerMenu unused

--- src/main/java/zhenyuan/sneakplayermenu/SneakPlayerMenu.java
+++ src/main/java/zhenyuan/sneakplayermenu/SneakPlayerMenu.java
@@ -7,6 +7,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import zhenyuan.sneakplayermenu.Listener.OnPlayerLeft;
 import zhenyuan.sneakplayermenu.Listener.SneakPlayerListener;
 
+@SuppressWarnings("unused")
 public final class SneakPlayerMenu extends JavaPlugin {
 
     @Override
