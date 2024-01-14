99b8d9c25d44178ac29d07ef8ebaa57548d5811f
Zhenyuan <awdrgyj0487@gmail.com>
2024-01-14 19:54:09 +0800

    Added Pay Options

--- src/main/java/zhenyuan/sneakplayermenu/Message/SneakPlayerMessage.java
+++ src/main/java/zhenyuan/sneakplayermenu/Message/SneakPlayerMessage.java
@@ -1,44 +1,36 @@
 package zhenyuan.sneakplayermenu.Message;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.entity.Player;
 
 public class SneakPlayerMessage {
     public static void sendSneakPlayerMessage(Player player, Player target) {
-        Component Message = Component.text().build();
-        //裝飾用
-        Message = Message.append(
-                Component.text("--------------------------------")
-                        .append(Component.newline())
-        );
-        //標題
-        Message = Message.append(
-                Component.text("玩家交互系統")
-                        .color(NamedTextColor.AQUA)
-                        .append(Component.newline())
-        );
-        //顯示目標玩家
-        Message = Message.append(
-                Component.text("目標玩家: ")
+        TextComponent message = Component.text()
+                .append(Component.text("--------------------------------"))
+                .append(Component.newline())
+                .append(Component.text("玩家交互系統", NamedTextColor.AQUA, TextDecoration.BOLD))
+                .append(Component.newline())
+                .append(Component.text("目標玩家: ")
                         .color(NamedTextColor.AQUA)
                         .append(Component.text(target.getName()))
                         .color(NamedTextColor.YELLOW)
                         .append(Component.newline())
-                        .append(Component.newline())
-        );
-        //操作面板
-        Message = Message.append(
-                Component.text("[私訊]")
+                        .append(Component.newline()))
+                .append(Component.text("[私訊] ")
                         .color(NamedTextColor.GREEN)
-                        .clickEvent(ClickEvent.suggestCommand("/m " + target.getName() + " "))
-                        .append(Component.newline())
-        );
-        //裝飾用
-        Message = Message.append(Component.text("--------------------------------"));
+                        .clickEvent(ClickEvent.suggestCommand("/m " + target.getName() + " ")))
+                .append(Component.text("[轉帳]")
+                        .color(NamedTextColor.LIGHT_PURPLE)
+                        .clickEvent(ClickEvent.suggestCommand("/pay " + target.getName() + " "))
+                        .append(Component.newline()))
+                .append(Component.text("--------------------------------"))
+                .build();
 
-        player.sendMessage(Message);
+        player.sendMessage(message);
 
     }
 }
